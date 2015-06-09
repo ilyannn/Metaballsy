b11b419cc145f124ba1ae3509c4996e1644329f9
ilya n <ilya.nikokoshev@gmail.com>
2015-06-09 10:52:34 +0300

    labels

--- Metaballsy/ViewController.m
+++ Metaballsy/ViewController.m
@@ -16,6 +16,13 @@
 @property (weak, nonatomic) IBOutlet UISlider *thresholdSlider;
 @property (weak, nonatomic) IBOutlet UISlider *gooinessSlider;
 
+@property (weak, nonatomic) IBOutlet UILabel *radiusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *speedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *thresholdLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gooinessLabel;
+
+
 @property (weak, nonatomic) IBOutlet MetalBallView *metalBallView;
 @end
 
@@ -31,24 +38,33 @@
     [self gooinessChanged:self];
 }
 
+- (void)updateLabel:(UILabel *)label fromSlider:(UISlider *)slider {
+    label.text = [NSString stringWithFormat:@"%.1f", slider.value];
+}
+
 - (IBAction)radiusChanged:(id)sender {
-    self.metalBallView.radius = self.radiusSlider.value; 
+    self.metalBallView.radius = self.radiusSlider.value;
+    [self updateLabel:self.radiusLabel fromSlider:self.radiusSlider];
 }
 
 - (IBAction)speedChanged:(id)sender {
     self.metalBallView.speed = self.speedSlider.value; 
+    [self updateLabel:self.speedLabel fromSlider:self.speedSlider];
 }
 
 - (IBAction)distanceChanged:(id)sender {
     self.metalBallView.distance = self.distanceSlider.value; 
+    [self updateLabel:self.distanceLabel fromSlider:self.distanceSlider];
 }
 
 - (IBAction)thresholdChanged:(id)sender {
     self.metalBallView.threshold = self.thresholdSlider.value; 
+    [self updateLabel:self.thresholdLabel fromSlider:self.thresholdSlider];
 }
 
 - (IBAction)gooinessChanged:(id)sender {
     self.metalBallView.gooiness = self.gooinessSlider.value; 
+    [self updateLabel:self.gooinessLabel fromSlider:self.gooinessSlider];
 }
 
 
